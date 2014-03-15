19e7f35b77741b937b9b3ea9852733b396bcb4a9
davidmesa <david.mesa.bustamante@gmail.com>
2014-03-14 22:07:11 -0500

    estructura de beans y de bos creada

---
+++ JEE/TeleConsulta/TeleConsulta-ejb/src/java/com/fsfb/bos/ReportePresionArterial.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fsfb.bos;
+
+/**
+ *
+ * @author davidmesa
+ */
+public class ReportePresionArterial {
+    
+}
---
+++ JEE/TeleConsulta/TeleConsulta-war/src/java/com/fsfb/teleconsulta/beans/LoginBean.java
@@ -0,0 +1,44 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * $Id$ CatalogoBean.java
+ * Universidad de los Andes (Bogotá - Colombia)
+ * Departamento de Ingeniería de Sistemas y Computación
+ * Licenciado bajo el esquema Academic Free License version 3.0
+ *
+ * Ejercicio: Muebles de los Alpes
+ * Autor: Juan Sebastián Urrego
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package com.fsfb.teleconsulta.beans;
+
+/**
+ * Managed bean encargado del catálogo de muebles en el sistema
+ * @author Juan Sebastián Urrego
+ */
+public class LoginBean
+{
+
+    //-----------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------
+
+    
+    
+    //-----------------------------------------------------------
+    // Constructor
+    //-----------------------------------------------------------
+
+    /**
+     * Constructor de la clase principal
+     */
+    public LoginBean()
+    {
+        
+    }
+
+    //-----------------------------------------------------------
+    // Getters y setters
+    //-----------------------------------------------------------
+
+}
